2884a5f0580e102bdcf1a0c7a4b7e6f0a279323d
ArtAlSh <105551538+ArtAlSh@users.noreply.github.com>
2022-12-14 22:10:48 +0200

    Update Common.js

--- src/Common.js
+++ src/Common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const HOME_LINK = "/";
+const HOME_LINK = "";
 // const BACK_URL = "http://127.0.0.1:8000/";
 // const BACK_URL = "http://localhost:8000/";
 const BACK_URL = "https://games-production-7379.up.railway.app/";
